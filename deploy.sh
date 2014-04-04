27f223d723dcefdf6ceee1859573bc30eb1a5c09
nhanb <thanhnhan483@gmail.com>
2014-04-04 23:43:45 +0700

    add token to git push url

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,7 @@
 # This script is for use in travis-ci only.
 # Humans like you have no business here!
 
-# Just kidding! Here's making sure github will let us push
-credentials_path=$HOME/credentials
-git config credential.helper "store --file=$credentials_path"
-echo "https://${GH_TOKEN}@github.com" > $credentials_path
+# Just kidding! Here's making sure git will let us push
 git config --global user.email "nhan.buithanh@rmitc.org"
 git config --global user.name "travis"
 
@@ -17,4 +14,4 @@ chmod +x ghp-import  # make it executable
 
 # Push gh-pages branch from this repo to production repo's master branch.
 # Pretty neat eh?
-git push https://github.com/rmitc/rmitc.github.io.git gh-pages:master
+git push https://${GH_TOKEN}@github.com/rmitc/rmitc.github.io.git gh-pages:master
