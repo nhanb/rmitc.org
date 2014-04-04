c8e4d4117937cfaf840f3fbdcdc8503c7f5a0657
nhanb <thanhnhan483@gmail.com>
2014-04-05 06:47:28 +0700

    hide $GH_TOKEN in git credential config

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
 
 # This script is for use in travis-ci only.
-# Humans like you have no business here!
+# Puny humans like you have no business here!
 
 # Just kidding! Here's making sure git will let us push
 git config --global user.email "nhan.buithanh@rmitc.org"
 git config --global user.name "travis"
 
-# Fetch ghp-import script (should have stuck with Pelican...)
+# Clone existing production repo and change working dir to it
+git config credential.https://github.com.username $GH_TOKEN
+source_path=`pwd`  # store current working dir's path
+production_path=$HOME/production
+production_repo=https://github.com/rmitc/rmitc.github.io.git
+git clone $production_repo $production_path
+cd $production_path
+
+# Fetch ghp-import script (python FTW!)
 wget https://raw.githubusercontent.com/davisp/ghp-import/master/ghp-import
 chmod +x ghp-import  # make it executable
-./ghp-import _site  # dump generated web files to gh-pages branch
 
-# Push gh-pages branch from this repo to production repo's master branch.
-# Pretty neat eh?
-git push https://${GH_TOKEN}@github.com/rmitc/rmitc.github.io.git gh-pages:master
+# dump generated web files to master branch then push
+#   -n: add .nojekyll file
+#   -p: push to remote (origin)
+#   -b: specify branch
+#   -m: commit message
+./ghp-import -p -n -b master -m "Build #$TRAVIS_BUILD_NUMBER" $source_path/_site
