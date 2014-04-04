bc76019ddb86af37dcf7f38be2ac1b78fc886f5a
nhanb <thanhnhan483@gmail.com>
2014-04-04 22:26:05 +0700

    Fix deploy.sh, add build status badge to README

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,15 @@
 # This script is for use in travis-ci only.
 # Humans like you have no business here!
 
-git config credential.helper "store --file=~/credentials"
-echo "https://${GH_TOKEN}:@github.com" > ~/credentials
+$credentials_path = "$HOME/credentials"
+git config credential.helper "store --file=$credentials_path"
+echo "https://${GH_TOKEN}@github.com" > $credentials_path
 git config --global user.email "nhan.buithanh@rmitc.org"
 git config --global user.name "travis"
 
-git clone https://github.com/rmitc/rmitc.github.io.git ~/output
-jekyll build --destination ~/output
+site_path=$HOME/rmitc_site
+git clone https://github.com/rmitc/rmitc.github.io.git $site_path
+jekyll build --destination $site_path
 cd ~/output
 git add -A
 git commit -m $TRAVIS_BUILD_NUMBER
