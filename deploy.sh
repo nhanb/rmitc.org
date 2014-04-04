9b27383ba8d20bb50a560a86b83d583cb4b3876d
nhanb <thanhnhan483@gmail.com>
2014-04-04 22:11:00 +0700

    add git user info for travis

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@
 
 git config credential.helper "store --file=~/credentials"
 echo "https://${GH_TOKEN}:@github.com" > ~/credentials
+git config --global user.email "nhan.buithanh@rmitc.org"
+git config --global user.name "travis"
+
 git clone https://github.com/rmitc/rmitc.github.io.git ~/output
 jekyll build --destination ~/output
 cd ~/output
